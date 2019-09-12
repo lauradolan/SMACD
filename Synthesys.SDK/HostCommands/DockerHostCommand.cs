511661ce939b49d4d1e133fff292223001a39c40
Anthony Turner <225599+anthturner@users.noreply.github.com>
2019-09-12 13:30:57 -0700

    HostCommand feature
    Will replace ExecutionWrapper to permit more flexible execution and a better way of handling Docker containers

---
+++ Synthesys.SDK/HostCommands/DockerHostCommand.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Synthesys.SDK.HostCommands
+{
+    public class DockerHostMount
+    {
+        public string LocalPath { get; set; }
+        public string ContainerPath { get; set; }
+        public bool IsReadOnly { get; set; }
+    }
+
+    public class DockerHostCommand : HostCommand, IDisposable
+    {
+        public DockerHostCommand(string image, IEnumerable<DockerHostMount> mounts, string user) : this(
+            new CreateContainerParameters
+            {
+                Image = image,
+                User = user,
+                AttachStderr = true,
+                AttachStdout = true,
+                AttachStdin = false,
+                HostConfig = new HostConfig
+                {
+                    Binds = mounts.Select(m => $"{m.LocalPath}:{m.ContainerPath}:{(m.IsReadOnly ? "ro" : "rw")}")
+                        .ToList()
+                }
+            })
+        {
+        }
+
+        public DockerHostCommand(CreateContainerParameters containerParameters)
+        {
+            ContainerParameters = containerParameters;
+        }
+
+        public CreateContainerParameters ContainerParameters { get; }
+        public string ContainerId { get; private set; }
+
+        public async Task Start()
+        {
+            DockerClient client;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                client = new DockerClientConfiguration(new Uri("npipe://./pipe/docker_engine")).CreateClient();
+            else
+                client = new DockerClientConfiguration(new Uri("unix:///var/run/docker.sock")).CreateClient();
+
+            var container = await client.Containers.CreateContainerAsync(ContainerParameters);
+            ContainerId = container.ID;
+
+            await client.Containers.StartContainerExecAsync(ContainerId);
+
+            var stdoutStream = await client.Containers.AttachContainerAsync(ContainerId, false,
+                new ContainerAttachParameters
+                {
+                    Stdout = true,
+                    Stderr = false,
+                    Stdin = false,
+                    Stream = true
+                });
+            var stderrStream = await client.Containers.AttachContainerAsync(ContainerId, false,
+                new ContainerAttachParameters
+                {
+                    Stdout = false,
+                    Stderr = true,
+                    Stdin = false,
+                    Stream = true
+                });
+            //var multiplexedStream = await client.Containers.StartAndAttachContainerExecAsync(ContainerId, false);
+
+            Task.Run(() => ReadLoop(stdoutStream, s => HandleStdOut(s)));
+            Task.Run(() => ReadLoop(stderrStream, s => HandleStdErr(s)));
+        }
+
+        private async Task ReadLoop(MultiplexedStream multiplexedStream, Action<string> action)
+        {
+            var dockerBuffer = new byte[4096];
+            try
+            {
+                while (true)
+                {
+                    Array.Clear(dockerBuffer, 0, dockerBuffer.Length);
+                    var dockerReadResult = await multiplexedStream.ReadOutputAsync(dockerBuffer, 0, dockerBuffer.Length,
+                        default);
+
+                    if (dockerReadResult.EOF)
+                        break;
+
+                    if (dockerReadResult.Count > 0)
+                    {
+                        var segment = new ArraySegment<byte>(dockerBuffer, 0, dockerReadResult.Count);
+                        action(Encoding.ASCII.GetString(segment.Array, 0, segment.Count));
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, "Failure during Read from Docker Exec to WebSocket");
+            }
+        }
+
+
+        #region IDisposable Support
+
+        private bool disposedValue; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~DockerHostCommand()
+        // {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
---
+++ Synthesys.SDK/HostCommands/HostCommand.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Synthesys.SDK.HostCommands
+{
+    public abstract class HostCommand
+    {
+        public delegate void ExternalProcessDataReceived(object sender, int ownerTaskId, string data);
+
+        /// <summary>
+        ///     A collection of maps between ManagedThreadId (from ExecutionWrapper) and TaskId (from plugin)
+        /// </summary>
+        public static ConcurrentDictionary<int, int> Maps = new ConcurrentDictionary<int, int>();
+
+        protected HostCommand()
+        {
+            OwnerTaskId = Task.CurrentId.GetValueOrDefault(-1);
+            Logger = Global.LogFactory.CreateLogger(GetType().Name);
+        }
+
+        protected ILogger Logger { get; }
+        protected int OwnerTaskId { get; }
+
+        /// <summary>
+        ///     Task wrapping this execution
+        /// </summary>
+        public Task RuntimeTask { get; protected set; } = Task.FromResult(0);
+
+        /// <summary>
+        ///     Duration of last execution
+        /// </summary>
+        public TimeSpan ExecutionTime { get; protected set; }
+
+        /// <summary>
+        ///     Process object executing this command
+        /// </summary>
+        private Process Process { get; } = new Process();
+
+        /// <summary>
+        ///     If STDOUT should be captured and stored in StdOut property
+        /// </summary>
+        public bool CaptureStdOut { get; set; }
+
+        /// <summary>
+        ///     If STDERR should be captured and stored in StdErr property
+        /// </summary>
+        public bool CaptureStdErr { get; set; }
+
+        /// <summary>
+        ///     Standard output from last execution
+        /// </summary>
+        public string StdOut { get; protected set; }
+
+        /// <summary>
+        ///     Standard error from last execution
+        /// </summary>
+        public string StdErr { get; protected set; }
+
+        /// <summary>
+        ///     If this process failed to execute
+        /// </summary>
+        public bool FailedToExecute { get; protected set; }
+
+        /// <summary>
+        ///     Fired when data is written to STDOUT
+        /// </summary>
+        public event ExternalProcessDataReceived StandardOutputDataReceived;
+
+        /// <summary>
+        ///     Fired when data is written to STDERR
+        /// </summary>
+        public event ExternalProcessDataReceived StandardErrorDataReceived;
+
+        protected void HandleStdOut(string text)
+        {
+            if (CaptureStdOut) StdOut += $"{text}{Environment.NewLine}";
+            StandardOutputDataReceived?.Invoke(this, OwnerTaskId, text);
+        }
+
+        protected void HandleStdErr(string text)
+        {
+            if (CaptureStdErr) StdErr += $"{text}{Environment.NewLine}";
+            StandardErrorDataReceived?.Invoke(this, OwnerTaskId, text);
+        }
+    }
+}
